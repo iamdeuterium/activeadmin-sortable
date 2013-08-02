661114f356817ab68dda7d756361ec6e2782a9f8
Stanislav Fesenko <iamdeuterium@gmail.com>
2013-08-02 22:40:48 +0400

    Usable version.

--- app/assets/javascripts/activeadmin-sortable.js
+++ app/assets/javascripts/activeadmin-sortable.js
@@ -1,24 +1,33 @@
-//= require jquery-ui
-
-(function($) {
-  $(document).ready(function() {
-    $('.handle').closest('tbody').activeAdminSortable();
-  });
-
-  $.fn.activeAdminSortable = function() {
-    this.sortable({
-      update: function(event, ui) {
-        var url = ui.item.find('[data-sort-url]').data('sort-url');
-
-        $.ajax({
-          url: url,
-          type: 'post',
-          data: { position: ui.item.index() + 1 },
-          success: function() { window.location.reload() }
-        });
-      }
+(function ($) {
+    $(document).ready(function () {
+        $('.handle').closest('tbody').activeAdminSortable();
     });
 
-    this.disableSelection();
-  }
+    $.fn.activeAdminSortable = function () {
+        this.sortable({
+            helper: function (e, tr) {
+                var originals = tr.children();
+                var helper = tr.clone();
+                helper.children().each(function (index) {
+                    $(this).width(originals.eq(index).width());
+                });
+                return helper;
+            },
+            handle: ".handle",
+            axis: "y",
+            placeholder: "ui-state-highlight",
+            update: function (event, ui) {
+                $.ajax({
+                    url: ui.item.find('[data-sort-url]').data('sort-url'),
+                    type: 'post',
+                    data: { position: ui.item.index() + 1 },
+                    success: function () {
+                        window.location.reload()
+                    }
+                });
+            }
+        });
+
+        this.disableSelection();
+    }
 })(jQuery);
